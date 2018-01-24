dd68b813e5742479a64b8f8022c50d1f7b0e5023
IwonaMaurer <plastimex2@wp.pl>
2018-01-24 11:24:51 +0100

    zadanie

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
+import routes from './routes';
 import { getCountries } from './actions/actions-countries';
+import './country.css';
 
 render(
     <Provider store={store}>
-        <h1>Inicjalizacja projektu</h1>
+         <div>
+         <Router history={hashHistory} routes={routes}>
+    
+         </Router>
+         <DevTools />
+     </div>
+       
     </Provider>,
     document.getElementById('root')
 );
-
-store.dispatch(getCountries());
